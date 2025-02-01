bb314d5200674063287fbac8e2b0ebbd58123a0d
Daniel Sena <daniel.desenvolvedor.io@gmail.com>
2025-02-01 09:26:59 -0300

    10 - Operadores artitméticos

--- Curso/ProjetoAulas/Program.cs
+++ Curso/ProjetoAulas/Program.cs
@@ -1,35 +1,14 @@
-﻿Console.WriteLine("Primeiro Programa");
+﻿int numero1 = 1;
+var numero2 = 2;
 
-int idade = 41;
-Console.WriteLine(idade);
+int soma = numero1 + numero2;
+Console.WriteLine(soma);
 
-string nomePessoa = "Daniel Sena";
-Console.WriteLine(nomePessoa);
+int subtracao = numero2 - numero1;
+Console.WriteLine(subtracao);
 
-decimal valor = 200.99m;
-Console.WriteLine(valor);
+int multiplicacao = (numero2 * numero1) * 10;
+Console.WriteLine(multiplicacao);
 
-double valorDouble = 200.99;
-Console.WriteLine(valorDouble);
-
-float valorFloat = 200.99f;
-Console.WriteLine(valorFloat);
-
-var novaIdade = 34;
-Console.WriteLine(novaIdade);
-
-char flag = 'y';
-Console.WriteLine(flag);
-
-bool ativo = false;
-ativo = true;
-Console.WriteLine(ativo);
-
-// Isso é uma constante
-const string descricao = "Curso C#";
-// descricao = "Curso";
-/*
-  Linha 1
-  Linha 2
-*/
-Console.WriteLine(descricao);
+int divisao = numero2 / numero1;
+Console.WriteLine(divisao);
