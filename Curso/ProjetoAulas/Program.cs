66f33efa8a9655be14e0f1e9ea6e46eafe8038e5
Daniel Sena <daniel.desenvolvedor.io@gmail.com>
2025-01-31 23:04:52 -0300

    07 - Trabalhando com variáveis

--- Curso/ProjetoAulas/Program.cs
+++ Curso/ProjetoAulas/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Primeiro Programa");
+
+int idade = 41;
+Console.WriteLine(idade);
+
+string nomePessoa = "Daniel Sena";
+Console.WriteLine(nomePessoa);
+
+decimal valor = 200.99m;
+Console.WriteLine(valor);
+
+double valorDouble = 200.99;
+Console.WriteLine(valorDouble);
+
+float valorFloat = 200.99f;
+Console.WriteLine(valorFloat);
+
+var novaIdade = 34;
+Console.WriteLine(novaIdade);
+
+char flag = 'y';
+Console.WriteLine(flag);
+
+bool ativo = false;
+Console.WriteLine(ativo);
